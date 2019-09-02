0e51655208cf66964a1f833b88e01b380c0ef296
Erick Lascalla <erick.lascalla@gmail.com>
2019-09-02 00:15:49 -0400

    Ajuste no app

--- src/pages/Meetups/index.js
+++ src/pages/Meetups/index.js
@@ -60,15 +60,8 @@ export default function Meetups() {
   }
 
   useEffect(() => {
-    loadPage();
-  }, []); // eslint-disable-line
-
-  useEffect(() => {
-    setPage(1);
-    setTotal(0);
     setMeetups([]);
-
-    loadPage();
+    loadPage(1, true);
   }, [date]); // eslint-disable-line
 
   function decrementDate() {
